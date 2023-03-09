831ebc6ede13adaabc49b1988b0ec91d9dfaf0b4
Qianzhi Wang <108285775+Vapson@users.noreply.github.com>
2023-03-09 15:03:57 +0800

    Update ReturnPeriod_change.py

--- ReturnPeriod_change.py
+++ ReturnPeriod_change.py
@@ -10,23 +10,30 @@ import basefunc
 
 def f_normal(inputs):
     """
-                motorway et.al return period/year、tertiary return period/year
-    high income：10、5
-    upper middle income：5、2
-    low middle/low income：2、0
+    According to lower design standards assumption (see supplementary Table S1)
+    
+                motorway et.al return period/year, tertiary, railways design return period/year
+    high income：10, 5, 30
+    upper middle income：5, 2 ,20
+    low middle/low income：2, 0, 10
+    
+    inputs[0]: income group index. 1: low income group, 2: lower middle income group, 3: upper middle income group, 4: high income group
+    inputs[1]: assets_type
+    inputs[2:]: new return period in the future. 0: 1.1years, 1: 2years, 2: 5years, 3: 10years, 4: 20years, 5: 30years, 6: 50years, 7: 100years 
+    
+    output: the proportion of new return period to the historical design return period
     """
-    #new_rt[]👉 0: 1.1years、1: 2years、2: 5years、3: 10years、4: 20years、5: 30years、6: 50years 7: 100years 
     income=inputs[0]
     assets_type=inputs[1]
     new_rt=inputs[-8:]
     y=-1.0
     if assets_type==1:
-        if income==1 or income==2: #low/lower middle
-            y=(new_rt[1]-2.0)/2.0 #median: x.index[1]                               
-        elif income==3: #upper middle
-            y=(new_rt[2]-5.0)/5.0 #median: x.index[2]  
-        elif  income==4: #high
-            y=(new_rt[3]-10.0)/10.0 #median: x.index[3]  
+        if income==1 or income==2: 
+            y=(new_rt[1]-2.0)/2.0                           
+        elif income==3: 
+            y=(new_rt[2]-5.0)/5.0 
+        elif  income==4: 
+            y=(new_rt[3]-10.0)/10.0  
         else:
             y=-1.0   
     elif assets_type==2:
